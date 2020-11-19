b5a52175f5a96576cc06c9d5dd65c1f106af9633
Rodrigo Sobisch <rolox05@gmail.com>
2020-11-18 21:40:15 -0300

    unit tests micronaut sin mock

--- tpaPrueba/src/test/java/tpaPruebaMicronaut/AutorControllerTest.java
+++ tpaPrueba/src/test/java/tpaPruebaMicronaut/AutorControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import javax.inject.Inject;
 
+import io.micronaut.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import io.micronaut.http.HttpRequest;
@@ -22,20 +23,60 @@ public class AutorControllerTest {
     RxHttpClient client;
 
     @Test
-    void testAutor(){
+    void testSaveAutor() {
         Autor testSave = new Autor();
-        testSave.setNombre("Peter");
-        testSave.setApellido("Pan");
-        testSave.setBiografia("Peter Pan es un eterno nino que vive en el pais de Nunca Jamas y desde alli nos cuenta maravillosas historias.");
+        testSave.setNombre("Pablo");
+        testSave.setApellido("Neruda");
+        testSave.setBiografia("Pablo Neruda, seudónimo y posterior nombre legal de Ricardo Eliécer Neftalí Reyes Basoalto (Parral, 12 de julio de 1904-Santiago de Chile, 23 de septiembre de 1973), fue un poeta y político chileno.");
 
         Flowable<HttpResponse<Autor>> result = client.exchange(HttpRequest.POST("/save", testSave), Autor.class);
 
         Autor autor = result.blockingFirst().getBody().get();
 
-        assertEquals("Peter", autor.getNombre());
-        assertEquals("Pan", autor.getApellido());
-        assertEquals("Peter Pan es un eterno nino que vive en el pais de Nunca Jamas y desde alli nos cuenta maravillosas historias.", autor.getBiografia());
-
+        assertEquals("Pablo", autor.getNombre());
+        assertEquals("Neruda", autor.getApellido());
+        assertEquals("Pablo Neruda, seudónimo y posterior nombre legal de Ricardo Eliécer Neftalí Reyes Basoalto (Parral, 12 de julio de 1904-Santiago de Chile, 23 de septiembre de 1973), fue un poeta y político chileno.", autor.getBiografia());
     }
 
+    @Test
+    void testGetAllAutores() {
+        Flowable<HttpResponse<Iterable>> result = client.exchange(HttpRequest.GET("/"), Iterable.class);
+        assertEquals(HttpStatus.OK, result.blockingFirst().getStatus());
+    }
+
+    @Test
+    void testGetOneAutor() {
+        Flowable<HttpResponse<Autor>> result = client.exchange(HttpRequest.GET("/1"), Autor.class);
+
+        Autor autor = result.blockingFirst().getBody().get();
+
+        assertEquals(HttpStatus.OK, result.blockingFirst().getStatus());
+        assertEquals("Pablo", autor.getNombre());
+        assertEquals("Neruda", autor.getApellido());
+        assertEquals("Pablo Neruda, seudónimo y posterior nombre legal de Ricardo Eliécer Neftalí Reyes Basoalto (Parral, 12 de julio de 1904-Santiago de Chile, 23 de septiembre de 1973), fue un poeta y político chileno.", autor.getBiografia());
+    }
+
+    @Test
+    void testUpdateAutor() {
+        Autor testUpdate = new Autor();
+        testUpdate.setId(1L);
+        testUpdate.setNombre("Ricardo");
+        testUpdate.setApellido("Reyes");
+
+        Flowable<HttpResponse<Autor>> result = client.exchange(HttpRequest.PUT("/1", testUpdate), Autor.class);
+
+        Autor autor = result.blockingFirst().getBody().get();
+
+        assertEquals(HttpStatus.OK, result.blockingFirst().getStatus());
+        assertEquals("Pablo", autor.getNombre());
+        assertEquals("Neruda", autor.getApellido());
+        assertEquals("Pablo Neruda, seudónimo y posterior nombre legal de Ricardo Eliécer Neftalí Reyes Basoalto (Parral, 12 de julio de 1904-Santiago de Chile, 23 de septiembre de 1973), fue un poeta y político chileno.", autor.getBiografia());
+    }
+
+    @Test
+    void testDeleteAutor() {
+        Flowable<HttpResponse<Autor>> result = client.exchange(HttpRequest.DELETE("delete/1"), Autor.class);
+
+        assertEquals(HttpStatus.OK, result.blockingFirst().getStatus());
+    }
 }
